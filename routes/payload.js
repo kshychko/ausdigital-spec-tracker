613bb95feff57f8df6d90349f265396b808e857d
Kseniya <k.shychko@gmail.com>
2017-02-03 21:50:23 +0300

    adding payload route for webhooks

--- routes/payload.js
+++ routes/payload.js
@@ -5,8 +5,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
-console.log("Received:\n", req.body)
+router.post('/', function (req, res, next) {
+    console.log("Received:\n", req.body)
+    res.send('webhook was received');
 });
 
 module.exports = router;
