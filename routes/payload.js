f3aaf9b79cb22939841b093d95f55ad28e32dc75
Kseniya <k.shychko@gmail.com>
2017-02-05 06:04:29 +0300

    clean-up spec files before update

--- routes/payload.js
+++ routes/payload.js
@@ -16,31 +16,37 @@ var logger = log4js.getLogger('app');
 
 /* GET home page. */
 router.post('/', function (req, res, next) {
-    console.log("Received:\n")
-    var repoURL = req.body.repository.git_url;
-    var repoName = req.body.repository.name;
-    var authorEmail = req.body.head_commit.author.email;
-    var authorName = req.body.head_commit.author.name;
-    var commitMessage = req.body.head_commit.message;
-    logger.log("repoURL - ", repoURL);
-    logger.log("repoName - ", repoName);
-    logger.log("authorEmail - ", authorEmail);
-    logger.log("authorName - ", authorName);
-    logger.log("commitMessage - ", commitMessage);
-    exec('sh sh/git-pull.sh'
-        + ' -n ' + repoName
-        + ' -u ' + repoURL
-        + ' -a "' + authorName + '"'
-        + ' -b ' + authorEmail
-        + ' -c "' + commitMessage + '"'
-        + ' -t ' + 'ausdigital.github.io'
-        + ' -r ' + 'git@github.com:kshychko/ausdigital.github.io.git'
-        , function (err, stdout, stderr) {
-            logger.error(err)
-            logger.log(stdout)
-            logger.error(stderr)
-            res.send('webhook was received');
-        });
+    var eventType = req.get('X-GitHub-Event');
+    if ( eventType == 'push') {
+        console.log("Push Received:\n")
+        var repoURL = req.body.repository.git_url;
+        var repoName = req.body.repository.name;
+        var authorEmail = req.body.head_commit.author.email;
+        var authorName = req.body.head_commit.author.name;
+        var commitMessage = req.body.head_commit.message;
+        logger.log("repoURL - ", repoURL);
+        logger.log("repoName - ", repoName);
+        logger.log("authorEmail - ", authorEmail);
+        logger.log("authorName - ", authorName);
+        logger.log("commitMessage - ", commitMessage);
+        exec('sh sh/git-pull.sh'
+            + ' -n ' + repoName
+            + ' -u ' + repoURL
+            + ' -a "' + authorName + '"'
+            + ' -b ' + authorEmail
+            + ' -c "' + commitMessage + '"'
+            + ' -t ' + 'ausdigital.github.io'
+            + ' -r ' + 'git@github.com:kshychko/ausdigital.github.io.git'
+            , function (err, stdout, stderr) {
+                logger.error(err)
+                logger.log(stdout)
+                logger.error(stderr)
+                res.send('webhook was received');
+            });
+    } else {
+        console.log("Push Received:\n")
+        res.send(eventType + ' was received');
+    }
 });
 
 module.exports = router;
